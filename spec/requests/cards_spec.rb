6e7c0574be603d03d86a02f3d36c5db351267b96
Murilo Mothsin <mothsin@hotmail.com>
2024-09-02 12:07:08 -0300

    card specs [not working yet]

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -3,24 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Cards' do
+  include Rack::Test::Methods
+  before do
+    Company.create(name: 'asd', cnpj: '123')
+    User.create(name: 'asd', email: 'asd@asd.com', password: '123456', company: Company.last, role: :admin)
+    post login_path, :email => 'asd@asd.com', :password => '123456'
+
+  end
   describe 'GET /index' do
     it 'returns http success' do
-      get '/cards/index'
+      get '/cards.json'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /create' do
+  describe 'POST /create' do
     it 'returns http success' do
-      get '/cards/create'
+      Company.create(name: 'asd', cnpj: '123')
+      User.create(name: 'asd', email: 'asd@asd.com', company: Company.last)
+      post cards_path, {
+        card: {
+          last4: '1234',
+          user_id: User.last.id
+        }
+      }
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /update' do
-    it 'returns http success' do
-      get '/cards/update'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe 'GET /update' do
+  #   it 'returns http success' do
+  #     get '/cards/update'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
